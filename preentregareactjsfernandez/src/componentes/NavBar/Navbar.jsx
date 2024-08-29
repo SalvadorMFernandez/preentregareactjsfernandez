02cdee39a11b77871ccc8c880d4cd079361bc296
Salvador Fernandez <salvadormatiasfernandez@gmail.com>
2024-08-29 08:42:36 -0300

    actualizacion

---
+++ preentregareactjsfernandez/src/componentes/NavBar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <h1>Tienda Onlines</h1>
+
+        <nav>
+            <li></li>
+            <li></li>
+            <li></li>
+        </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
